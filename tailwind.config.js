aa3ba394b739a91be89984ebf82ed7f556434e6a
Jared <jacallantine@crimson.ua.edu>
2025-01-09 18:54:41 -0600

    test

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html","./HTML/**/*.html", "./JS/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
